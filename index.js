67429e1dc314ba3039228e232dbf368474bef481
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-16 07:22:37 +0300

    create profile route

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import protectedRoutes from "./routes/protected.js";
 import blogRoutes from "./routes/blogs.js";
+import profileRoutes from "./routes/profile.js";
+
 
 dotenv.config();
 
 const app = express();
 
-
+app.use("/api/profile", profileRoutes);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
